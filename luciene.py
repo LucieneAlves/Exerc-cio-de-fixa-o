f918cab1b9f32af3f8ad4b74dbe529b22d0c8436
LucieneAlves <lusantos_adm@hotmail.com>
2022-08-31 22:19:27 -0300

    python

---
+++ luciene.py
@@ -0,0 +1,8 @@
+def cadastro_cliente():
+    nome = input('digite nome do cliente')
+    endereco = input('digite endereço do cliente')
+    contato = input('digite contato da cliente')
+    contato = '+55' + contato
+    print(contato)
+
+desc_produto = input('digite nome do produto')
